8f6ad40e6e5d013ae56b2ab038519c5113c2f107
Abdul <abdulkaleem8360@gmail.com>
2024-05-04 12:20:50 +0530

    Added 1581st leetcode problem

---
+++ CustomerWhoVisitedbutDidNotMakeAnyTransactions(1581).sql
@@ -0,0 +1,3 @@
+select v.customer_id,count(v.customer_id) as count_no_trans from Visits v 
+left join  Transactions t on v.visit_id=t.visit_id
+ where t.transaction_id is null group by customer_id; 
