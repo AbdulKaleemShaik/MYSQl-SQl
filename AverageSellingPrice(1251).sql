37aac8b6599436a07041945a5b3ac0a20cc93ae1
Abdul <abdulkaleem8360@gmail.com>
2024-05-07 09:11:40 +0530

    Added 1251st leetcode problem

---
+++ AverageSellingPrice(1251).sql
@@ -0,0 +1,5 @@
+select p.product_id,ifnull(round((sum(u.units*p.price)/sum(u.units)),2),0) 
+as average_price from
+Prices p left join UnitsSold u on p.product_id=u.product_id and
+u.purchase_date between p.start_date and p.end_date 
+group by product_id;
